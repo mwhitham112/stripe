e71aee13910faa27a25ee886be4908ade398a8eb
mwhitham112 <134304879+mwhitham112@users.noreply.github.com>
2023-07-18 12:05:48 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const env = require("dotenv").config({ path: "./.env" });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
+app.use(express.static(process.env.STATIC_DIR));
+app.use(express.json())
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const money = req.body.totalPrice
+  const final = Math.round(money * 100);
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "GBP",
+      amount: final,
+      automatic_payment_methods: { enabled: true },
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(5252, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
